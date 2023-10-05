48fc6fe77959ded6e3cc0cd74c91a0065ea9aa22
zmeng <ayafcc@gmail.com>
2023-10-06 01:21:17 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -P 28809 ./app/build/outputs/apk/release/sweetieplalyer_latest.apk xman1@74.120.170.55:~/public/
+scp -P 28809 ./app/build/outputs/apk/release/sweetieplalyer_latest.apk xman1@74.120.170.55:~/download/
